1696d197b3ae7c5f197d02e346591de490b88c28
TdB_Snip3r <vincenzo.brunale@gmail.com>
2025-04-02 13:25:54 +0200

    Aggiunta di nuove dipendenze e aggiornamenti nel file pnpm-lock.yaml per i pacchetti payments e test. Rimosse dipendenze obsolete e aggiornate versioni di alcune librerie.

---
+++ packages/wallet/postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: {
+    // Non includiamo 'tailwindcss' qui.
+    // Lasciamo l'oggetto plugins vuoto per ora.
+    // Se in futuro scoprissimo che @megotickets/core necessita
+    // specificamente di altri plugin PostCSS (es. autoprefixer),
+    // potranno essere aggiunti qui.
+  }
+}; 
