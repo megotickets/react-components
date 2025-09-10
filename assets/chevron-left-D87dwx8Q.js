0c3f03a6ec15c76445c561e860bf522c3b2f89f3
turinglabsorg <turinglabsorg@users.noreply.github.com>
2025-09-10 14:33:29 +0000

    deploy: c2f28ab95a1e0591b8e38bad1637003920ed6411

--- assets/chevron-left-D87dwx8Q.js
+++
@@ -1,8 +0,0 @@
-import{b as o}from"./core-Bm0hQt0d.js";import"./index-C6V0_5rb.js";import"./index.es-oUWFb4Lc.js";import"./events-DQ172AOg.js";const t=o`<svg fill="none" viewBox="0 0 16 16">
-  <path
-    fill="currentColor"
-    fill-rule="evenodd"
-    d="M11.04 1.46a1 1 0 0 1 0 1.41L5.91 8l5.13 5.13a1 1 0 1 1-1.41 1.41L3.79 8.71a1 1 0 0 1 0-1.42l5.84-5.83a1 1 0 0 1 1.41 0Z"
-    clip-rule="evenodd"
-  />
-</svg>`;export{t as chevronLeftSvg};
