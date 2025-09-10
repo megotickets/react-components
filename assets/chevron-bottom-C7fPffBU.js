1d1dd0d5045db20e77905be349be7375ae0921a9
turinglabsorg <turinglabsorg@users.noreply.github.com>
2025-09-10 05:06:38 +0000

    deploy: 68bbe51e29c0186d6711819192c955cb750e80a0

---
+++ assets/chevron-bottom-C7fPffBU.js
@@ -0,0 +1,8 @@
+import{b as o}from"./core-Bm0hQt0d.js";import"./index-C6V0_5rb.js";import"./index.es-oUWFb4Lc.js";import"./events-DQ172AOg.js";const i=o`<svg fill="none" viewBox="0 0 16 16">
+  <path
+    fill="currentColor"
+    fill-rule="evenodd"
+    d="M1.46 4.96a1 1 0 0 1 1.41 0L8 10.09l5.13-5.13a1 1 0 1 1 1.41 1.41l-5.83 5.84a1 1 0 0 1-1.42 0L1.46 6.37a1 1 0 0 1 0-1.41Z"
+    clip-rule="evenodd"
+  />
+</svg>`;export{i as chevronBottomSvg};
